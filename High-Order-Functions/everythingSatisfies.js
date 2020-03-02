d96ea14b65f953902f4d50fe5bd163c370ccc6bd
Jean Costa <jean.adcc@gmail.com>
2020-03-02 10:04:03 -0300

    Working on Everythings Satisfies

--- High-Order-Functions/everythingSatisfies.js
+++ High-Order-Functions/everythingSatisfies.js
@@ -1,19 +1,18 @@
 // every method
 function every(array, test) {
-  veredict = true;
   for (const element of array) {
-    if (!test(element)) veredict = false;
+    if (!test(element)) return false;
   }
-  return veredict;
+  return true;
 }
 
 function every2(array, test) {
   if (array.some(test)) return true;
 }
 
-console.log(every2([1, 3, 5], n => n < 10));
+console.log(every([1, 3, 5], n => n < 10));
 // → true
-console.log(every2([2, 4, 16], n => n < 10));
+console.log(every([2, 4, 16], n => n < 10));
 // → false
-console.log(every2([], n => n < 10));
+console.log(every([], n => n < 10));
 // → true
