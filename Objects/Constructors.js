8355c443cb5913de77f9e0eb05749f87d6c5feb3
Jean Costa <jean.adcc@gmail.com>
2020-03-02 14:12:02 -0300

    Contructors notes

---
+++ Objects/Constructors.js
@@ -0,0 +1,41 @@
+/*
+A class defines the shape of a type of object—what methods and properties
+ it has. Such an object is called an instance of the class.
+
+
+
+Prototypes are useful for defining properties for which all instances of
+ a class share the same value, such as methods. Properties that differ 
+ per instance, such as our rabbits’ type property, need to be stored 
+ directly in the objects themselves.
+ */
+
+// THE CONSTRUCTOR FUNCTION BACKBONE
+let protoRabbit = {
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+};
+function makeRabbit(type) {
+  let rabbit = Object.create(protoRabbit);
+  rabbit.type = type;
+  return rabbit;
+}
+makeRabbit("Killer Rabbit");
+
+//Using the "new" keyword before, a function is treated as a consctructor
+function Rabbit(type) {
+  this.type = type;
+}
+Rabbit.prototype.speak = function(line) {
+  console.log(`The ${this.type} rabbit says '${line}'`);
+};
+
+let weirdRabbit = new Rabbit("weird");
+// By convention, the names of constructors are capitalized so that they
+// can easily be distinguished from other functions.
+
+console.log(Object.getPrototypeOf(Rabbit) == Function.prototype);
+// → true
+console.log(Object.getPrototypeOf(weirdRabbit) == Rabbit.prototype);
+// → true
