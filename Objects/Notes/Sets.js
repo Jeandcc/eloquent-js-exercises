fb75d4bcd1f7b056e40ebe82be34c327da2acc02
Jean Costa <jean.adcc@gmail.com>
2020-03-04 14:40:37 -0300

    Sets Notes

---
+++ Objects/Notes/Sets.js
@@ -0,0 +1,9 @@
+/*
+
+The standard JavaScript environment provides another data structure called Set.
+Like an instance of Map, a set holds a collection of values. Unlike Map, it does
+not associate other values with those—it just tracks which values are part of
+the set. A value can be part of a set only once—adding it again doesn’t have any
+effect.
+
+*/
