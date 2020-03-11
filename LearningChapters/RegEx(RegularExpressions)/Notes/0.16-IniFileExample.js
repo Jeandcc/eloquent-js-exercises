86054a36798d43731e2b18a75c84a16d83b943ed
Jean Costa <jean.adcc@gmail.com>
2020-03-11 11:46:05 -0300

    REGEX - Init File Parsing

---
+++ LearningChapters/RegEx(RegularExpressions)/Notes/0.16-IniFileExample.js
@@ -0,0 +1,48 @@
+let iniFile = `
+searchengine=https://duckduckgo.com/?q=$1
+spitefulness=9.7
+
+; comments are preceded by a semicolon...
+; each section concerns an individual enemy
+[larry]
+fullname=Larry Doe
+type=kindergarten bully
+website=http://www.geocities.com/CapeCanaveral/11451
+
+[davaeorn]
+fullname=Davaeorn
+type=evil wizard
+outputdir=/home/marijn/enemies/davaeorn
+
+`;
+
+/*
+The rules: 
+- Blank lines and lines starting with semicolons are ignored.
+- Lines wrapped in [ and ] start a new section.
+- Lines containing an alphanumeric identifier followed by an = character add a
+  setting to the current section.
+- Anything else is invalid.
+
+Observation: Some operating systems use not just a newline character to separate
+ lines but a carriage return character followed by a newline ("\r\n")
+*/
+
+function parseINI(string) {
+  // Start with an object to hold the top-level fields
+  let result = {};
+  let section = result;
+  string.split(/\r?\n/).forEach(line => {
+    let match;
+    if ((match = line.match(/^(\w+)=(.*)$/))) {
+      section[match[1]] = match[2];
+    } else if ((match = line.match(/^\[(.*)\]$/))) {
+      section = result[match[1]] = {};
+    } else if (!/^\s*(;.*)?$/.test(line)) {
+      throw new Error("Line '" + line + "' is not valid.");
+    }
+  });
+  return result;
+}
+
+parseINI(iniFile);
